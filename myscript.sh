f7a3c1f0704397a8bef45a98df8248afcef18d52
silverspoon2000 <163788999+silverspoon2000@users.noreply.github.com>
2024-11-21 19:45:25 -0500

    conflicting change before local commit

--- myscript.sh
+++ myscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "hello world"
-echo "new changes"
+echo "new changes2"
 
-echo " this line was added after local commit, check for conflicts!!!"
+echo " this line was added after local commit, check for conflicts!!asdfasdf!"
